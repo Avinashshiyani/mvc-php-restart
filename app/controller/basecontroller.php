2dd7e60fff041e4a9c01b5252fdc864ac64dc9a8
avinash <avinashshiyani760@example.com>
2023-08-08 17:52:21 +0530

    started mvc

---
+++ app/controller/basecontroller.php
@@ -0,0 +1,12 @@
+
+<?php
+
+
+class basecontroller
+{
+  public function index(){
+    $title = 'Welcome to our framework';
+    view('welcome',compact('title'));
+  }
+  
+}
---
+++ app/controller/userscontroller.php
@@ -0,0 +1,22 @@
+<?php
+
+class userscontroller 
+{
+  public $user_model;
+
+  public function __construct(){
+    model('user');
+    $this->user_model = new User();
+  }
+  public function index()
+  {
+    $users =  $this->user_model->allusers();
+    view('users', compact('users'));
+  }
+
+  public function delete($id)
+  {
+    $data =  $this->user_model->delete($id);
+    echo $data;
+  }
+}
